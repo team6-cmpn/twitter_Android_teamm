2c98763e22bc38001d2713aad45750fd0aa14beb
Ammar <ammar.abdelnabi00@eng-st.cu.edu.eg>
2022-04-26 08:42:32 +0200

    unit-testing + validation

---
+++ twitter_app/lib/model/text_field_validation.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: file_names, unused_import
+
+import 'package:flutter/material.dart';
+
+/* class NameFieldValidator {
+  NameFieldValidator(String? value);
+
+  static String? validate(String value) {
+    if (value.isEmpty || !RegExp(r'^[a-z A-Z]').hasMatch(value)) {
+      return "Enter correct Name";
+    } else {
+      return null;
+    }
+  }
+} */
+
+class emailValidator {
+  emailValidator(String value);
+
+  static String validate(String value) {
+    if (value.isEmpty ||
+        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}').hasMatch(value)) {
+      return "Enter correct email";
+    } else {
+      return null;
+    }
+  }
+}
+
+class nameValidator {
+  nameValidator(String value);
+
+  static String validate(String value) {
+    if (value.isEmpty || !RegExp('[A-Za-z0-9_]').hasMatch(value)) {
+      return "Enter correct name Alphabticals and underscore only";
+    } else {
+      return null;
+    }
+  }
+}
+
+class PassValidator {
+  PassValidator(String value);
+
+  static String validate(String value) {
+    if (value.isEmpty || !RegExp(r'^[a-z A-Z 0-9 $]+$').hasMatch(value)) {
+      return "Enter correct Password";
+    } else {
+      return null;
+    }
+  }
+}
+
+/* class ForgetPassFieldValidator {
+  ForgetPassFieldValidator(String? value);
+
+  static String? validate(String value) {
+    if (value.isEmpty ||
+        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}').hasMatch(value)) {
+      return "Enter correct email";
+    } else {
+      return null;
+    }
+  }
+}
+
+class DateOfBirthFieldValidator {
+  DateOfBirthFieldValidator(String? value);
+  static String? validate(String value) {
+    if (value.isEmpty) {
+      return 'Enter correct date of birth';
+    } else {
+      return null;
+    }
+  }
+}
+
+class VerifyEmailFieldValidator {
+  VerifyEmailFieldValidator(String? value);
+
+  static String? validate(String value) {
+    if (value.isEmpty || !RegExp(r'^[0-9]+$').hasMatch(value)) {
+      return "Enter correct code";
+    } else {
+      return null;
+    }
+  }
+}
+
+class SetPassFieldValidator {
+  SetPassFieldValidator(String? value);
+
+  static String? validate(String value) {
+    if (value.isEmpty || !RegExp(r'^[a-z A-Z 0-9]+$').hasMatch(value)) {
+      return "Enter correct Password";
+    } else {
+      return null;
+    }
+  }
+}
+
+class SetUsernameFieldValidator {
+  SetUsernameFieldValidator(String? value);
+  static String? validate(String value) {
+    if (value.isEmpty || !RegExp(r'^[a-z A-Z 0-9]').hasMatch(value)) {
+      return "Enter correct Username";
+    } else {
+      return null;
+    }
+  }
+}
+ */
